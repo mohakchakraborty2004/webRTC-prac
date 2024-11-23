16dd1507452a28c3b597440f1c1efe562e7fbbb7
mohak <c.mohak2004@gmail.com>
2024-11-24 00:09:00 +0530

    added rtc logic , ice candidates remains

--- frontend/frontend/src/pages/reciever.tsx
+++ frontend/frontend/src/pages/reciever.tsx
@@ -1,4 +1,36 @@
-export default function Sender() {
+import { useEffect, useState } from "react";
+import useSocket from "../hooks/useSocket";
 
+export default function Reciever() {
+
+    const socket = useSocket();
     
+
+    useEffect(() => {
+        if(!socket) return;
+        
+        socket.send(JSON.stringify({type: "reciever"}));
+        
+        HandleSocket(socket);
+   
+    }, [])
+
+    const HandleSocket = async (Socket : WebSocket) => {
+
+        const pc = new RTCPeerConnection();
+
+        Socket.onmessage = async(event) => {
+            const message =  JSON.parse(event.data);
+            if (message.type === "createOffer") {
+            await pc.setRemoteDescription(message.sdp);
+            const answer = await pc.createAnswer()
+            await pc.setLocalDescription(answer)
+            Socket.send(JSON.stringify({
+                type : "createAnswer", 
+                sdp : answer
+            }))
+            }
+        }
+    }
+
 }
--- frontend/frontend/src/pages/sender.tsx
+++ frontend/frontend/src/pages/sender.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSocket from "../hooks/useSocket"
 
 const Sender = () => {
    
     const socket = useSocket();
+    const [pc , setPc] = useState<RTCPeerConnection | null>(null)
 
     useEffect(()=> {
    
@@ -13,18 +14,36 @@ const Sender = () => {
 
         socket.send(JSON.stringify({type : "sender"})); 
 
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
 
-            if(message.type === "createAnswer"){
-
-            }
-        }
 
     }, [])
 
-    const HandleMessage = () => {
-        socket?.send("")
+    const HandleMessage = async () => {
+        if (!socket){
+            return;
+        }
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+
+            if(message.type === "createAnswer"){
+           await pc?.setRemoteDescription(message.sdp)
+            }
+        }
+
+        const PC = new RTCPeerConnection();
+        setPc(PC);
+
+
+         PC.onnegotiationneeded = async() => {
+            const offer =  await PC.createOffer();
+            await PC.setLocalDescription(offer)
+            socket.send(JSON.stringify({
+                type : "createOffer",
+                sdp : PC.localDescription
+            }))
+         }
+        
     }
 
     return <>
